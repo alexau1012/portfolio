2ca341afe7a3867dc97975308b0cc8434da785eb
Alex Au <alexaumh@gmail.com>
2021-08-03 09:14:28 +1000

    Implement intro component

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,7 +3,21 @@ import "./intro.scss"
 export default function intro() {
     return (
         <div className="intro" id="intro">
-            
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/profile-picture.jpg" alt=""/>
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Alex Au</h1>
+                    <h3>Software Engineer<span></span></h3>
+                </div>                
+                <a href="portfolio">
+                    <img src="assets/down.png" alt=""/>
+                </a>
+            </div>            
         </div>
     )
 }
--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -2,6 +2,7 @@ import "./menu.scss"
 import "../menuItem/MenuItem";
 import MenuItem from "../menuItem/MenuItem";
 
+// eslint-disable-next-line
 export default function ({ menuOpen, setMenuOpen }) {
     let menuItems = [
         "intro",
