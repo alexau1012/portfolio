1adbc3799dfff3e01fade19e1a4f3482fbbe0271
Alex <alexaumh@gmail.com>
2021-08-14 22:27:17 +1000

    Fix education formatting

--- src/components/education/Education.jsx
+++ src/components/education/Education.jsx
@@ -8,18 +8,20 @@ export default function Education() {
             <div className="container">
                     {data.map(d =>
                         <div className="card">
-                            <h2>{d.school}</h2>
-                            <h3>{d.degree}</h3>
-                            <h4>{d.duration}</h4>
-                            <h5>
-                                {d.gpa ? `GPA: ${d.gpa}` : ""}
-                            </h5>
-                            <ul>
-                                {d.achievements?.map(da =>     
-                                    <li>{da}</li>
-                                )}
-                            </ul>
-                            <img src={d.img} alt="" />
+                            <div className="info">
+                                <h2>{d.school}</h2>
+                                <h3>{d.degree}</h3>
+                                <h4>{d.duration}</h4>
+                                <h5>
+                                    {d.gpa ? `GPA: ${d.gpa}` : ""}
+                                </h5>
+                                <ul>
+                                    {d.achievements?.map(da =>     
+                                        <li>{da}</li>
+                                    )}
+                                </ul>
+                                <img src={d.img} alt="" />
+                            </div>
                         </div>
                     )}
             </div>
