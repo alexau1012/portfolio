9c174bc4f96286bfb024c75cb5892de48000f19f
Alex <alexaumh@gmail.com>
2021-08-15 09:21:19 +1000

    Fix incorrect redirection of skills view

--- src/components/menuItem/MenuItem.jsx
+++ src/components/menuItem/MenuItem.jsx
@@ -1,10 +1,15 @@
 import "./menuItem.scss"
 
 export default function MenuItem({ name, setMenuOpen }) {
+
+    const selectName = (name) => {
+        return name === "skills" ? "education": name;
+    }
+
     return (
         <div className="menuItem">
             <li onClick={() => setMenuOpen(false)}>
-                <a href={"#" + name}>{name}</a>
+                <a href={"#" + selectName(name)}>{name}</a>
             </li>
         </div>
     )
